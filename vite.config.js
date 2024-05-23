3a56838f74cadb96c39dc286b7fc320af9764032
cxashish <ashish.culturex@gmail.com>
2024-05-23 13:54:38 +0530

    CHORE : adding tailwindcss and shadcn ui

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,26 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export default defineConfig(() => {
-    return {
-        server: {
-            port: 3003,
-            open: true,
+// Convert import.meta.url to a file path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+    server: {
+        port: 3003,
+        open: true,
+    },
+    build: {
+        outDir: "build",
+    },
+    envPrefix: "REACT_APP_",
+    plugins: [react(), svgr({ svgrOptions: { icon: true } })],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
         },
-        build: {
-            outDir: "build",
-        },
-        envPrefix: "REACT_APP_",
-        plugins: [react(), svgr({ svgrOptions: { icon: true } })],
-        resolve: {
-            alias: [{ find: "~", replacement: resolve(__dirname, "./src") }],
-        },
-    };
+    },
 });
