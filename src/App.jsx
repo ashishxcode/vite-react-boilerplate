14e82903893acd7021d8349892bc447dcd3a5215
ashishxcode <ashish.patel0903@gmail.com>
2024-05-26 23:36:07 +0530

    CHORE : Improve the app.jsx ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 const App = () => {
     return (
-        <div className=" flex h-svh flex-col items-center justify-center space-y-4">
-            <h1 className="text-3xl font-bold">
-                Welcome to Vite + React Boilerplate
+        <div className="flex h-svh flex-col items-center justify-center space-y-4 bg-slate-950 text-slate-100">
+            <h1 className="space-x-2 text-4xl font-bold">
+                <span className="inline-block bg-gradient-to-b from-pink-600  to-indigo-400 bg-clip-text text-transparent">
+                    Vite
+                </span>
+                <span>+</span>
+                <span className="inline-block bg-gradient-to-b from-blue-600  to-indigo-400 bg-clip-text text-transparent">
+                    React
+                </span>
+                <span>+</span>
+                <span className="inline-block bg-gradient-to-b from-slate-100  to-slate-400 bg-clip-text text-transparent">
+                    ShadCN
+                </span>
             </h1>
-            <p className="text-xl font-medium">Tailwindcss + Shadcn UI</p>
+
+            <p className="font-medium ">
+                Get started by editing <code>src/App.jsx</code>
+            </p>
         </div>
     );
 };
